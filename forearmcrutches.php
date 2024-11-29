3b76aed2b98deb7b03ec4956df37830b7b62450f
joksdemic <jovanademic2@gmail.com>
2024-11-29 19:51:59 +0100

    Create crutches content and update images on shop page

---
+++ forearmcrutches.php
@@ -0,0 +1,123 @@
+<?php require_once 'C:\\xampp\\htdocs\\webshop\\helpers.php'; ?>
+
+<?php loadPartial('head'); ?>
+<?php loadPartial('navbar'); ?>
+
+<div class="wheelchairs-h1"><h1>Podlakatne štake</h1></div>
+
+<div class="wheelchairs1">
+    <div class="wheelchairs1-content" data-product-id="61">
+        <img src="./public/assets/forearmcrutches/forearmcrutches1.webp" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">1.950,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="61">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="1950">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="62">
+        <img src="./public/assets/forearmcrutches/forearmcrutches2.webp" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">1.100,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="62">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="1100">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="63">
+        <img src="./public/assets/forearmcrutches/forearmcrutches3.jpg" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">2.590,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="63">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="2590">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="64">
+        <img src="./public/assets/forearmcrutches/forearmcrutches4.jpg" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">1.150,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="64">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="1150">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="65">
+        <img src="./public/assets/forearmcrutches/forearmcrutches5.jpg" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">2.700,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="65">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="2700">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="66">
+        <img src="./public/assets/forearmcrutches/forearmcrutches6.png" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">2.300,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="66">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="2300">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="67">
+        <img src="./public/assets/forearmcrutches/forearmcrutches7.webp" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">2.500,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="67">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="2500">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="68">
+        <img src="./public/assets/forearmcrutches/forearmcrutches8.jpg" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">2.400,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="68">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="2400">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="69">
+        <img src="./public/assets/forearmcrutches/forearmcrutches9.jpg" alt="">
+        <div class="name">PODLAKATNE ŠTAKE DEČIJE</div>
+        <div class="price">1.790,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="69">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE DEČIJE">
+            <input type="hidden" name="product_price" value="1790">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+</div>
+
+<?php loadPartial('scroll')?>
+<?php loadPartial('footer'); ?>
+<script src="public/js/script.js"></script>
+
+</body>
+</html>
---
+++ underarmcrutches.php
@@ -0,0 +1,125 @@
+<?php require_once 'C:\\xampp\\htdocs\\webshop\\helpers.php'; ?>
+
+<?php loadPartial('head'); ?>
+<?php loadPartial('navbar'); ?>
+
+<div class="wheelchairs-h1"><h1>Podpazušne štake</h1></div>
+
+<div class="wheelchairs1">
+
+    <div class="wheelchairs1-content" data-product-id="70">
+        <img src="./public/assets/underarmcrutches/underarmcrutches1.webp" alt="">
+        <div class="name">PODPAZUŠNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">1.950,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="70">
+            <input type="hidden" name="product_name" value="PODPAZUŠNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="1950">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="71">
+        <img src="./public/assets/underarmcrutches/underarmcrutches2.jpg" alt="">
+        <div class="name">PODPAZUŠNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">1.100,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="71">
+            <input type="hidden" name="product_name" value="PODPAZUŠNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="1100">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="72">
+        <img src="./public/assets/underarmcrutches/underarmcrutches3.webp" alt="">
+        <div class="name">PODPAZUŠNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">2.590,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="72">
+            <input type="hidden" name="product_name" value="PODPAZUŠNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="2590">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="73">
+        <img src="./public/assets/underarmcrutches/underarmcrutches4.webp" alt="">
+        <div class="name">PODPAZUŠNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">1.150,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="73">
+            <input type="hidden" name="product_name" value="PODPAZUŠNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="1150">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="74">
+        <img src="./public/assets/underarmcrutches/underarmcrutches5.jpg" alt="">
+        <div class="name">PODPAZUŠNE ŠTAKE LIGHT ALLOY</div>
+        <div class="price">2.700,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="74">
+            <input type="hidden" name="product_name" value="PODPAZUŠNE ŠTAKE LIGHT ALLOY">
+            <input type="hidden" name="product_price" value="2700">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="75">
+        <img src="./public/assets/underarmcrutches/underarmcrutches6.webp" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">2.300,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="75">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="2300">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="76">
+        <img src="./public/assets/underarmcrutches/underarmcrutches7.jpg" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">2.500,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="76">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="2500">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="77">
+        <img src="./public/assets/underarmcrutches/underarmcrutches8.webp" alt="">
+        <div class="name">PODLAKATNE ŠTAKE ALUMINIJUMSKE</div>
+        <div class="price">2.400,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="77">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE ALUMINIJUMSKE">
+            <input type="hidden" name="product_price" value="2400">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="78">
+        <img src="./public/assets/underarmcrutches/underarmcrutches9.jpg" alt="">
+        <div class="name">PODLAKATNE ŠTAKE DEČIJE</div>
+        <div class="price">1.790,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="78">
+            <input type="hidden" name="product_name" value="PODLAKATNE ŠTAKE DEČIJE">
+            <input type="hidden" name="product_price" value="1790">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+</div>
+
+<?php loadPartial('scroll')?>
+<?php loadPartial('footer'); ?>
+
+<script src="public/js/script.js"></script>
+
+</body>
+</html>

b8b9abe4a5bd44cb6c2951753b28b0834f304a3e
joksdemic <jovanademic2@gmail.com>
2024-11-29 14:40:26 +0100

    Add functionality to a slider

---
+++ public/js/script.js
@@ -0,0 +1,55 @@
+//SLIDER
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide');
+
+    if (slides && totalSlides.length > 0) {
+        let currentSlide = 0;
+
+        function changeSlide(direction) {
+            currentSlide += direction;
+
+            if (currentSlide < 0) {
+                currentSlide = totalSlides.length - 1;
+            } else if (currentSlide >= totalSlides.length) {
+                currentSlide = 0;
+            }
+
+            const offset = -currentSlide * 100;
+            slides.style.transform = `translateX(${offset}%)`;
+
+            updateButtonVisibility();
+        }
+
+        function autoChangeSlide() {
+            changeSlide(1);
+        }
+
+        setInterval(autoChangeSlide, 2000);
+
+        function updateButtonVisibility() {
+            const previousButton = document.querySelector('.previous');
+            const nextButton = document.querySelector('.next');
+            if (previousButton) {
+                previousButton.disabled = currentSlide === 0;
+            }
+            if (nextButton) {
+                nextButton.disabled = currentSlide === totalSlides.length - 1;
+            }
+        }
+
+        const prevButton = document.querySelector('.previous');
+        const nextButton = document.querySelector('.next');
+
+        if (prevButton) {
+            prevButton.addEventListener('click', () => changeSlide(-1));
+        }
+        if (nextButton) {
+            nextButton.addEventListener('click', () => changeSlide(1));
+        }
+
+        updateButtonVisibility();
+    } else {
+        console.error("Slides not found in the DOM.");
+    }
+});

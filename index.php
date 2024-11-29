484a4ccebfff35cb3c4d81b0b157955fec8711a5
joksdemic <jovanademic2@gmail.com>
2024-11-29 14:36:55 +0100

    Create and style slider

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php require_once 'C:\\xampp\\htdocs\\webshop\\helpers.php'; ?>
 
-<?php loadPartial('head'); ?>
+<?php loadPartial('head');?>
 <?php loadPartial('navbar');?>
+<?php loadPartial('slider');?>
 
 
 <script src="./public/js/script.js"></script>
---
+++ views/partials/slider.php
@@ -0,0 +1,23 @@
+<div class="slider">
+    <div class="slides">
+        <div class="slide">
+            <img src="./public/assets/slider/slide1.PNG" alt="Slika 1">
+        </div>
+        <div class="slide">
+            <img src="./public/assets/slider/slide2.jpg" alt="Slika 2">
+        </div>
+        <div class="slide">
+            <img src="./public/assets/slider/slide3.jpg" alt="Slika 3">
+        </div>
+        <div class="slide">
+            <img src="./public/assets/slider/slide5.jpg" alt="Slika 4">
+        </div>
+        <div class="slide">
+            <img src="./public/assets/slider/slide6.jpg" alt="Slika 5">
+        </div>
+    </div>
+
+    <button class="previous" onclick="changeSlide(-1)"><img width="48" height="48" src="https://img.icons8.com/pulsar-line/48/back.png" alt="back"/></button>
+    <button class="next" onclick="changeSlide(1)"><img width="48" height="48" src="https://img.icons8.com/pulsar-line/48/forward.png" alt="forward"/></button>
+</div>
+</div>             

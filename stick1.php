dfac254f5564ba90880198f92a2e96a704a47786
joksdemic <jovanademic2@gmail.com>
2024-11-29 21:21:06 +0100

    Create stick content

---
+++ stick1.php
@@ -0,0 +1,125 @@
+<?php require_once 'C:\\xampp\\htdocs\\webshop\\helpers.php'; ?>
+
+<?php loadPartial('head'); ?>
+<?php loadPartial('navbar'); ?>
+
+<div class="wheelchairs-h1"><h1>Štapovi na produženje</h1></div>
+
+<div class="wheelchairs1">
+
+    <div class="wheelchairs1-content" data-product-id="70">
+        <img src="./public/assets/stick1/stick1_1.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE SA BAZOM I LED</div>
+        <div class="price">1.500,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="79">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE SA BAZOM I LED">
+            <input type="hidden" name="product_price" value="1500">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="80">
+        <img src="./public/assets/stick1/stick1_2.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE</div>
+        <div class="price">1.500,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="80">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE">
+            <input type="hidden" name="product_price" value="1500">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="81">
+        <img src="./public/assets/stick1/stick1_3.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE</div>
+        <div class="price">3.590,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="81">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE">
+            <input type="hidden" name="product_price" value="3590">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="82">
+        <img src="./public/assets/stick1/stick1_4.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE</div>
+        <div class="price">6.150,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="82">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE">
+            <input type="hidden" name="product_price" value="6150">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="83">
+        <img src="./public/assets/stick1/stick1_5.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE</div>
+        <div class="price">3.700,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="83">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE">
+            <input type="hidden" name="product_price" value="3700">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="84">
+        <img src="./public/assets/stick1/stick1_6.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE</div>
+        <div class="price">2.300,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="84">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE">
+            <input type="hidden" name="product_price" value="2300">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="85">
+        <img src="./public/assets/stick1/stick1_7.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE</div>
+        <div class="price">2.500,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="85">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE">
+            <input type="hidden" name="product_price" value="2500">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="86">
+        <img src="./public/assets/stick1/stick1_8.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE</div>
+        <div class="price">4.400,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="86">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE">
+            <input type="hidden" name="product_price" value="4400">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="87">
+        <img src="./public/assets/stick1/stick1_9.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE</div>
+        <div class="price">5.790,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="87">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE">
+            <input type="hidden" name="product_price" value="5790">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+</div>
+
+<?php loadPartial('scroll')?>
+<?php loadPartial('footer'); ?>
+
+<script src="public/js/script.js"></script>
+
+</body>
+</html>
---
+++ stick3.php
@@ -0,0 +1,125 @@
+<?php require_once 'C:\\xampp\\htdocs\\webshop\\helpers.php'; ?>
+
+<?php loadPartial('head'); ?>
+<?php loadPartial('navbar'); ?>
+
+<div class="wheelchairs-h1"><h1>Štapovi sa četiri tačke oslonca</h1></div>
+
+<div class="wheelchairs1">
+
+    <div class="wheelchairs1-content" data-product-id="97">
+        <img src="./public/assets/stick3/stick3_1.jpg" alt="">
+        <div class="name">ŠTAP ČETVOROKRAKI FS 922</div>
+        <div class="price">1.500,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="97">
+            <input type="hidden" name="product_name" value="ŠTAP ČETVOROKRAKI FS 922">
+            <input type="hidden" name="product_price" value="1500">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="98">
+        <img src="./public/assets/stick3/stick3_2.jpg" alt="">
+        <div class="name">ŠTAP ČETVOROKRAKI FTS 342</div>
+        <div class="price">1.500,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="98">
+            <input type="hidden" name="product_name" value="ŠTAP ČETVOROKRAKI FTS 342">
+            <input type="hidden" name="product_price" value="1500">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="99">
+        <img src="./public/assets/stick3/stick3_03.jpg" alt="">
+        <div class="name">ŠTAP ČETVOROKRAKI MOBIAK TIP 922</div>
+        <div class="price">3.590,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="99">
+            <input type="hidden" name="product_name" value="ŠTAP ČETVOROKRAKI MOBIAK TIP 922">
+            <input type="hidden" name="product_price" value="3590">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="100">
+        <img src="./public/assets/stick3/stick3_4.jpg" alt="">
+        <div class="name">ŠTAP ČETVOROKRAKI 76</div>
+        <div class="price">6.150,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="100">
+            <input type="hidden" name="product_name" value="ŠTAP ČETVOROKRAKI 76">
+            <input type="hidden" name="product_price" value="6150">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="101">
+        <img src="./public/assets/stick3/stick3_5.jpg" alt="">
+        <div class="name">ŠTAP NA PRODUŽENJE SILVER</div>
+        <div class="price">3.700,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="101">
+            <input type="hidden" name="product_name" value="ŠTAP NA PRODUŽENJE SILVER">
+            <input type="hidden" name="product_price" value="3700">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="102">
+        <img src="./public/assets/stick3/stick3_6.jpg" alt="">
+        <div class="name">ŠTAP ČETVOROKRAKI FDSFS 68</div>
+        <div class="price">2.300,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="102">
+            <input type="hidden" name="product_name" value="ŠTAP ČETVOROKRAKI FDSFS 68">
+            <input type="hidden" name="product_price" value="2300">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="103">
+        <img src="./public/assets/stick3/stick3_7.jpg" alt="">
+        <div class="name">ŠTAP ČETVOROKRAKI BLUE</div>
+        <div class="price">2.500,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="103">
+            <input type="hidden" name="product_name" value="ŠTAP ČETVOROKRAKI BLUE">
+            <input type="hidden" name="product_price" value="2500">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="104">
+        <img src="./public/assets/stick3/stick3_8.jpg" alt="">
+        <div class="name">ŠTAP ČETVOROKRAKI M</div>
+        <div class="price">2.500,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="104">
+            <input type="hidden" name="product_name" value="ŠTAP ČETVOROKRAKI M">
+            <input type="hidden" name="product_price" value="2500">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+
+    <div class="wheelchairs1-content" data-product-id="105">
+        <img src="./public/assets/stick3/stick3_9.jpg" alt="">
+        <div class="name">ŠTAP ČETVOROKRAKI S</div>
+        <div class="price">5.790,00 rsd.</div>
+        <form method="post" action="cart.php">
+            <input type="hidden" name="product_id" value="105">
+            <input type="hidden" name="product_name" value="ŠTAP ČETVOROKRAKI S">
+            <input type="hidden" name="product_price" value="5790">
+            <button type="submit" class="buy">Dodaj u korpu</button>
+        </form>
+    </div>
+</div>
+
+<?php loadPartial('scroll')?>
+<?php loadPartial('footer'); ?>
+
+<script src="public/js/script.js"></script>
+
+</body>
+</html>

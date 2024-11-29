d81ab3f44e58ca1c7ad38b57b55d194c0a3b6a78
joksdemic <jovanademic2@gmail.com>
2024-11-29 14:28:48 +0100

    Create and style navbar

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once 'C:\\xampp\\htdocs\\webshop\\helpers.php'; ?>
 
 <?php loadPartial('head'); ?>
-
+<?php loadPartial('navbar');?>
 
 
 <script src="./public/js/script.js"></script>
---
+++ views/partials/navbar.php
@@ -0,0 +1,16 @@
+<div class="nav">
+
+    <div class="logo"><img height="30px" src="./public/assets/wheelchair.png" alt="Logo"></div>
+        
+    <ul class="nav-nav">
+        <li class="nav-item"><a href="index.php">Home</a></li>
+        <li class="nav-item"><a href="shop.php">Prodavnica</a></li>
+        <li class="nav-item"><a href="aboutUs.php">O nama</a></li>
+        <li class="nav-item"><a href="contact.php">Kontakt</a></li>
+    </ul>  
+
+    <div class="cart">
+        <a href="cart.php"><img width="33" height="33" src="https://img.icons8.com/glyph-neue/64/1A1A1A/shopping-cart-loaded.png" alt="shopping-cart-loaded"/></a>
+    </div>
+</div>
+</div>

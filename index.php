78ac614d039417c89c1682d0bafc96ec99100dfc
joksdemic <jovanademic2@gmail.com>
2024-11-29 14:54:32 +0100

    Add content to main page

--- index.php
+++ index.php
@@ -4,6 +4,40 @@
 <?php loadPartial('navbar');?>
 <?php loadPartial('slider');?>
 
+<div class="index-content">
+
+    <div class="about">
+        <h3>Naši proizvodi su namenjeni ljudima svih uzrasta koji su se našli u nepredviđenim životnim okolnostima. 
+            Nudimo visok kvalitet po povoljnim cenama. Strategija preduzeća je širenje prodaje na teritoriji čitave države
+            putem interneta, sticanje poverenja klijenata, kao i novih poslovnih partnera.</h3>
+    </div>
+
+    <h1>Deo naseg asortimana...</h1>
+
+    <div class="content-box">
+        <img src="./public/assets/assortment-preview/wheelchair-preview.jpg" alt="">
+        <img src="./public/assets/assortment-preview/wheelchair-preview2.jpg" alt="">
+        <img src="./public/assets/assortment-preview/wheelchair-preview3.jpg" alt="">
+        <img src="./public/assets/assortment-preview/wheelchair-preview4.jpg" alt="">
+    </div>
+
+    <div class="content-box">
+        <img src="./public/assets/assortment-preview/walker-preview2.jpg" alt="">
+        <img src="./public/assets/assortment-preview/walker-preview4.jpg" alt="">
+        <img src="./public/assets/assortment-preview/walker2-preview.jpg" alt="">
+        <img src="./public/assets/assortment-preview/rollator-walker-preview.jpg" alt="">
+        <img src="./public/assets/assortment-preview/rollator-walker2-preview.jpg" alt="">
+    </div>
+
+    <div class="content-box">
+        <img src="./public/assets/assortment-preview/stick-preview.jpg" alt="">
+        <img src="./public/assets/assortment-preview/stick2-preview.jpg" alt="">
+        <img src="./public/assets/assortment-preview/stick3-preview.jpg" alt="">
+        <img src="./public/assets/assortment-preview/stick4-preview.jpg" alt="">
+    </div>
+
+</div>
+
 
 <script src="./public/js/script.js"></script>
 </body>

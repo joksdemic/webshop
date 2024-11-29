23b257e1612c3be1b71c58da567c7501c09aab7e
joksdemic <jovanademic2@gmail.com>
2024-11-29 17:39:49 +0100

    Add 'about us' content

--- aboutUs.php
+++ aboutUs.php
@@ -4,6 +4,38 @@
 <?php loadPartial('navbar'); ?>
 
 
+<div class="aboutus-content">
+    <h3 id="animated-text">Preduzeće “Orthopedist” se bavi uvozom, prodajom na malo, veliko, kao i internet prodajom i 
+                           servisiranjem ortopedskih pomagala. Naši proizvodi  su namenjeni ljudima svih uzrasta koji su se
+                           našli u nepredviđenim životnim okolnostima. Nudimo visok kvalitet po povoljnim cenama. Strategija
+                           preduzeća je širenje prodaje na teritoriji čitave države putem interneta, sticanje poverenja 
+                           klijenata, kao i novih poslovnih partnera.</h3>
+</div>
+
+<div class="aboutus-content2">
+    <p>Prodajni asortiman čine invalidska kolica (aktivna, mehanička, aktivna mehanička, elektromotorna, dečija) , hodalice
+        (sa ili bez točkića), štake (podlaktne i podpazušne), štapovi (aluminijumski, štapovi sa tri ili četiri tačke oslonca),
+        toaletna pomagala (toaletna kolica, toaletne stolice, nastavnici za wc, stolice za tuš kabinu,daske za kadu, šamponjere
+        za ležeće korisnike),  medicinski i bolnički nameštaj (mehanički, električni i hidraulični kreveti, trapezi za krevet, 
+        stočići, uzglavlja, stalci za infuziju, električne i hidraulične dizalice za nepokretne), proizvodi iz antidekubitnog 
+        programa (dušeci, jastuci i hidrokoloidne komprese), kose i vertikalne platforme za stepenište sa i bez krivina, dizalice,
+        rampe, guseničari, aparati za plućne bolesnike (koncentratori kiseonika, inhalatori, pulsni oksimetri), programi za 
+        inkontinenciju (pelene za odrasle, kateteri, urin kese, urinali, mušeme, lopate i guske), stoma program (kolostoma diskovi
+        i kese), ortoze, i pojasevi. Pored toga naše preduzeće pruža usluge isporuke (naše ili kurirske službe), montaže i 
+        servisiranja invalidskih kolica, zamenu guma, popravku sedišta, naslona i papučica  na terenu. Sve cene su izložene na
+        web sajtu naše internet prodavnice. Obzirom na širok asortiman proizvoda koje nudimo one zavise od potreba kupaca. Ne 
+        postoje skriveni troškovi. Kako nam je sigurnost naših klijenata prioritet plaćanje prilikom internet kupovine se 
+        obavlja pouzećem, pri samom prijemu proizvoda. Ukoliko iznos kupovine premaši od 15 000 dinara troškove isporuke 
+        postaju naša, a ne vaša briga. Distributeri smo vodećih svetskih proizvođača ortopedskih pomagala iz SAD, Velsa,
+        Francuske, Italije, Austrije, Rumunije, Grčke, Švedske, Kine, Australije. Medicinski i bolnički nameštaj, kao i 
+        toaletna pomagala koja su deo našeg asortimana su brenda MORETTI iz Italije, invalidska kolica Panthera iz Švedske, 
+        hodalice, štake i štapovi brenda Suncare Medical iz Kine, ortoze i pojasevi Triamed iz Rumunije, TENA program za 
+        inkontinenciju je iz Australije, stoma program brenda ConvaTec iz Velsa, antidekubitalni program iz Francuske 
+        proizvođača Systam, platforme, dizalice, rampe i guseničari iz proizvođača Intano iz Italije i SANO iz Austrije, 
+        aparati za plućne bolesnike MOBIAK iz Grčke i INVACARE iz SAD.
+    </p>
+</div>
+
 
 <?php loadPartial('footer'); ?>
 <script src="public/js/script.js"></script>

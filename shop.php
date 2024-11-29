86f7f0b194617e2ec9a0044591fab41c9f80dedf
joksdemic <jovanademic2@gmail.com>
2024-11-29 17:29:51 +0100

    Create content for shop page

--- shop.php
+++ shop.php
@@ -3,6 +3,115 @@
 <?php loadPartial('head'); ?>
 <?php loadPartial('navbar'); ?>
 
+
+<div class="subcategory-content">
+
+    <div class="subcategory-section">
+        
+        <h1>INVALIDSKA KOLICA</h1>
+
+        <div class="subcategory" id="kolica-mehanicka">
+            <a href="wheelchairs1.php"><p>Mehanička kolica</p></a>
+        </div>
+
+        <div class="subcategory" id="kolica-elektromotorna">
+            <a href="wheelchairs2.php"><p>Elektromotorna kolica</p></a>
+        </div>
+    
+        <div class="subcategory" id="kolica-aktivna">
+            <a href="wheelchairs3.php"><p>Aktivna kolica</p></a>
+        </div>
+    
+    </div>
+
+    <div class="subcategory-section">
+        
+        <h1>HODALICE</h1>
+        
+        <div class="subcategory" id="hodalica-zglobna">
+            <a href="articulatedwalker.php"><p>Zglobna hodalica</p></a>
+        </div>
+    
+        <div class="subcategory" id="hodalica-rolator">
+            <a href="rollatorwalker.php"><p>Rolator hodalica</p></a>
+        </div>
+    
+        <div class="subcategory" id="hodalica-podlakatna">
+            <a href="forearmwalker.php"><p>Podlakatna hodalica</p></a>
+        </div>
+
+    </div>
+
+    <div class="subcategory-section">
+    
+        <h1>ŠTAKE</h1>
+    
+        <div class="subcategory" id="stake-podlakatne">
+            <a href="forearmcrutches.php"><p>Podlaktne štake</p></a>
+        </div>
+    
+        <div class="subcategory" id="stake-podpazusne">
+            <a href="underarmcrutches.php"><p>Podpazušne štake</p></a>
+        </div>
+  
+    </div>
+
+    <div class="subcategory-section">
+    
+        <h1>ŠTAPOVI</h1>
+    
+        <div class="subcategory" id="stap">
+            <a href="stick1.php"><p>Štap na produženje</p></a>
+        </div>
+    
+        <div class="subcategory" id="stap3">
+            <a href="stick2.php"><p>Štap sa tri tačke oslonca</p></a>
+        </div>
+    
+        <div class="subcategory" id="stap4">
+            <a href="stick3.php"><p>Štap sa četiri tačke oslonca</p></a>
+        </div>
+
+    </div>
+
+    <div class="subcategory-section">   
+
+        <h1>KREVETI</h1>
+    
+        <div class="subcategory" id="elektricni-krevet">
+            <a href="electricbed.php"><p>Električni krevet</p></a>
+        </div>
+    
+        <div class="subcategory" id="bolnicki-krevet">
+            <a href="hospitalbeds.php"><p>Bolnički krevet</p></a>
+        </div>
+    
+        <div class="subcategory" id="kucna-nega-krevet">
+            <a href="homecarebed.php"><p>Krevet za kućnu negu</p></a>
+        </div>
+
+    </div>
+
+    <div class="subcategory-section">
+
+        <h1>DIZALICE</h1>
+
+        <div class="subcategory" id="elektricna-dizalica">
+            <a href="electrichoist.php"><p>Električna dizalica</p></a>
+        </div>
+    
+        <div class="subcategory" id="hidraulicna-dizalica">
+            <a href="hydraulic.php"><p>Hidraulična dizalica</p></a>
+        </div>
+    
+        <div class="subcategory" id="dizalica-korpa">
+            <a href="sling.php"><p>Korpa za dizalicu</p></a>
+        </div
+    </div>
+</div>
+</div>
+
+
 <?php loadPartial('scroll'); ?>
 <?php loadPartial('footer'); ?>
 <script src="./public/js/script.js"></script>

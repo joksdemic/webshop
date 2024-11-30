bca5f5f62ea4e490858a467face4ac61e934fe41
joksdemic <jovanademic2@gmail.com>
2024-11-30 01:51:41 +0100

    Create 'thank you' page

--- thankYou.php
+++ thankYou.php
@@ -3,7 +3,13 @@
 <?php loadPartial('head'); ?>
 <?php loadPartial('navbar'); ?>
 
+<audio id="thankyou-sound" autoplay>
+  <source src="./public/assets/sound/cashier-quotka-chingquot-sound-effect-129698.mp3" type="audio/mp3">
+</audio>
 
+<div class="container-thankyou">
+    <img src="./public/assets/thankyou.gif" alt="">
+</div>
 
 <?php loadPartial('footer'); ?>
 <script src="public/js/script.js"></script>
